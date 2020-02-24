e1bfae75ef122e7982093976d3381d8e547c0222
test1swuser <test1swuser@gmail.com>
2020-02-24 20:27:56 +0100

    Update scritp_post_CentOS_nginx.sh

--- linux/scritp_post_CentOS_nginx.sh
+++ linux/scritp_post_CentOS_nginx.sh
@@ -10,3 +10,4 @@ systemctl enable nginx
 #docroot en nginx: /usr/share/nginx/html/index.html
 #docroot en apache: /var/www/html/index.html
 echo  '<html> <body> <p>El nombre del servidor es: '$(hostname)'</p> <p>Private IP: '$(ip address |grep 'inet.*eth0')'</p></body> </html>'>>/usr/share/nginx/html/index.html
+ln -s ../../doc/HTML /usr/share/nginx/html/VSI06
