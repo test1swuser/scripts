1bbb54e6c95844c18299c9338d894b9d7e39039d
test1swuser <test1swuser@gmail.com>
2020-02-24 15:34:42 +0100

    Create scritp_post_CentOS_nginx.sh

---
+++ linux/scritp_post_CentOS_nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Script post-suministro
+
+# Instala un servidor nginx y crea una pagina html
+
+yum install -y epel-release
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+#docroot en nginx: /usr/share/nginx/html/index.html
+#docroot en apache: /var/www/html/index.html
+echo  '<html> <body> <p>El nombre del servidor es: '$(hostname)'</p> <p>Private IP: '$(ip address |grep 'inet.*eth0')'</p></body> </html>'>>/usr/share/nginx/html/index.html
